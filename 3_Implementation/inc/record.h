fe48494f38aa4d8ad5a3d9142aefbb46239e4298
206819 <81298446+206819@users.noreply.github.com>
2021-04-16 12:18:29 +0530

    Update record.h

--- 3_Implementation/inc/record.h
+++ 3_Implementation/inc/record.h
@@ -1,1 +1,29 @@
+# ifndef __RECORD_H__
+# define __RECORD_H__
 
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct student
+{
+    char name[20];
+    char mobile[10];
+    int rollno;
+    char course[20];
+    char branch[20];
+}s[30];
+
+void menu(struct student s[]);
+
+void add(struct student s[]);
+
+void view(struct student s[]);
+
+void search(struct student s[]);
+
+void modify(struct student s[]);
+
+void deleterec(struct student s[]);
+
+#endif
